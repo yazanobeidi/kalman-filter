0f1dbfd2504930eebdbe971a7c8180307fbc6d2c
yazanobeidi <yazan@protonmail.ch>
2015-07-22 21:03:36 -0400

    Added license

--- runkalman.m
+++ runkalman.m
@@ -1,3 +1,19 @@
+%	Copyright 2015 Yazan Obeidi
+%
+%   This program is free software; you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation; either version 2 of the License, or
+%   (at your option) any later version.
+%
+%   This program is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+%
+%   You should have received a copy of the GNU General Public License along
+%   with this program; if not, write to the Free Software Foundation, Inc.,
+%   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+%
 %%% Kalman Filter Run Script
 % This script attemps to explore custom Kalman filter possibilities
 % - as opposed to entering these through command
@@ -42,4 +58,4 @@ ylim([0 inf]) % adjust y axis limits
 
 clear A xi Pi B uk wk Q R H t fitresult
 
-% Written by Yazan Obeidi July 2015, Princess Margaret Cancer Centre
+% Written by Yazan Obeidi July 2015 at the Princess Margaret Cancer Centre
